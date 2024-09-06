12817422edd041e9651d58e5ad1662a56f6df318
Ankur77720 <prajapatiankur8349@gmail.com>
2024-09-06 13:55:14 +0530

    google oauth done

---
+++ 025-googleoauth/app.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+},
+    function (accessToken, refreshToken, profile, done) {
+
+        console.log('profile:', profile);
+
+
+        return done(null, profile);
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Home</h1>
+        <a href="/auth/google">Login with Google</a>
+        `);
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: [ 'profile', 'email' ] })
+);
+
+app.get('/auth/google/callback',
+    (req, res, next) => {
+        console.log(req.query);
+        return next();
+    },
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+        res.redirect('/profile');
+    }
+);
+
+app.get('/profile', (req, res) => {
+
+
+    res.send(`<h1>Profile</h1><pre>${JSON.stringify(req.user, null, 2)}</pre>`);
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
